7f398b3a3e2b9dd9e71f02d2e1ac90ebce89a0d6
Brian Norman <brian@bnorm.com>
2016-06-25 13:17:42 -0500

    Update the search recipe presenter test to fix build

--- app/src/test/java/com/bnorm/barkeep/ui/recipe/search/SearchRecipePresenterTest.java
+++ app/src/test/java/com/bnorm/barkeep/ui/recipe/search/SearchRecipePresenterTest.java
@@ -3,6 +3,7 @@ package com.bnorm.barkeep.ui.recipe.search;
 import java.io.IOException;
 import java.util.Collections;
 
+import android.support.annotation.NonNull;
 import com.bnorm.barkeep.server.data.store.v1.endpoint.Endpoint;
 import com.bnorm.barkeep.server.data.store.v1.endpoint.model.CollectionResponseRecipe;
 import org.junit.Rule;
@@ -10,11 +11,6 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import rx.Scheduler;
-import rx.android.plugins.RxAndroidPluginsRule;
-import rx.android.plugins.RxAndroidSchedulersHook;
-import rx.plugins.RxJavaPluginsRule;
-import rx.plugins.RxJavaSchedulersHook;
 import rx.schedulers.Schedulers;
 
 import static org.mockito.Matchers.any;
@@ -23,27 +19,20 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SearchRecipePresenterTest {
-    @Rule public RxJavaPluginsRule rxJavaPluginsRule = new RxJavaPluginsRule(new RxJavaSchedulersHook() {
-        @Override
-        public Scheduler getIOScheduler() {
-            return Schedulers.immediate();
-        }
-    });
-    @Rule public RxAndroidPluginsRule rxAndroidPluginsRule = new RxAndroidPluginsRule(new RxAndroidSchedulersHook() {
-        @Override
-        public Scheduler getMainThreadScheduler() {
-            return Schedulers.immediate();
-        }
-    });
     @Rule public MockitoRule rule = MockitoJUnit.rule();
 
     @Mock SearchRecipeView view;
     @Mock Endpoint endpoint;
 
+    @NonNull
+    private static SearchRecipePresenter presenter(SearchRecipeView view, Endpoint endpoint) {
+        return new SearchRecipePresenter(view, endpoint, Schedulers.immediate(), Schedulers.immediate());
+    }
+
     @Test
     public void submit_networkFailure() throws Exception {
         // given
-        SearchRecipePresenter presenter = new SearchRecipePresenter(view, endpoint);
+        SearchRecipePresenter presenter = presenter(view, endpoint);
         when(endpoint.listRecipes()).thenThrow(new IOException());
 
         // when
@@ -56,7 +45,7 @@ public class SearchRecipePresenterTest {
     @Test
     public void submit() throws Exception {
         // given
-        SearchRecipePresenter presenter = new SearchRecipePresenter(view, endpoint);
+        SearchRecipePresenter presenter = presenter(view, endpoint);
 
         // todo(bnorm) eww...  smelly...
         Endpoint.ListRecipes listRecipes = mock(Endpoint.ListRecipes.class);
